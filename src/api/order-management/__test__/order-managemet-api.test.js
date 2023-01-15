9ac211a9893be1c05fa0d2bc4d3df1cf6f6ea24f
YKavee <ykaveesenanayake@gmail.com>
2023-01-15 12:39:22 +0530

    unit tests for order, category and user managements

--- src/api/order-management/__test__/order-managemet-api.test.js
+++ src/api/order-management/__test__/order-managemet-api.test.js
@@ -10,6 +10,28 @@ jest.mock("../../../service/order-management/order-management-service");
 
 describe("order-management route", () => {
   describe("order-management create order function", () => {
+    let mockGetAllOrders;
+    beforeEach(() => {
+      mockGetAllOrders = jest.fn().mockReturnValue([
+        {
+          _id: "1",
+          product_id: "11",
+          customer_id: "1",
+          order_status: "Packing",
+          total_price: 2000,
+          payment_type: "Cash",
+        },
+        {
+          _id: "2",
+          product_id: "12",
+          customer_id: "1",
+          order_status: "Packing",
+          total_price: 5000,
+          payment_type: "Cash",
+        },
+      ]);
+    });
+
     test("create order request", async () => {
       jest.spyOn(OrderManagementService, "createOrder").mockReturnValueOnce(
         Promise.resolve({
@@ -34,6 +56,16 @@ describe("order-management route", () => {
         })
         .set("Accept", "application/json");
       expect(response.status).toEqual(201);
+      expect(response.body).toBeDefined();
+    });
+
+    test("get all orders", async () => {
+      OrderManagementService.getAll = mockGetAllOrders;
+
+      const response = await request(app)
+        .get(`/orders`)
+        .set("Accept", "application/json");
+      expect(response.status).toEqual(200);
       expect(response.body).toBeDefined();
     });
   });
